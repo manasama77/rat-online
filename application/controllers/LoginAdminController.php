a5e3ea5b70b801ca40cbf61cbc5432bc52575127
Adam PM <adam.pm77@gmail.com>
2020-08-24 15:42:51 +0700

    update

--- application/controllers/LoginAdminController.php
+++ application/controllers/LoginAdminController.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class LoginAdminController extends CI_Controller {
+class LoginAdminController extends CI_Controller
+{
 
 	public function __construct()
 	{
@@ -36,32 +37,32 @@ class LoginAdminController extends CI_Controller {
 
 		$this->form_validation->set_rules($config);
 
-		if ($this->form_validation->run() == FALSE){
+		if ($this->form_validation->run() == FALSE) {
 			$this->load->view('login_admin');
-		}else{
+		} else {
 			$username = $this->input->post('username', TRUE);
 			$password = $this->input->post('password', TRUE);
 
-			if($this->_username_check($username) == FALSE){
-				redirect('login/admin','refresh');
-			}elseif($this->_password_check($username, $password) == FALSE){
-				redirect('login/admin','refresh');
-			}else{
+			if ($this->_username_check($username) == FALSE) {
+				redirect('login/admin', 'refresh');
+			} elseif ($this->_password_check($username, $password) == FALSE) {
+				redirect('login/admin', 'refresh');
+			} else {
 				$arr  = $this->mmain->get_user_info($username);
 
 				$data_session = [
-					UNQ.'id'                => $arr->row()->id,
-					UNQ.'nama'              => $arr->row()->nama,
-					UNQ.'id_jabatan'        => $arr->row()->id_jabatan,
-					UNQ.'nama_jabatan'      => $arr->row()->nama_jabatan,
-					UNQ.'user_login'        => $arr->row()->user_login,
-					UNQ.'foto'              => $arr->row()->foto,
-					UNQ.'flag_ketua_sidang' => $arr->row()->flag_ketua_sidang,
+					UNQ . 'id'                => $arr->row()->id,
+					UNQ . 'nama'              => $arr->row()->nama,
+					UNQ . 'id_jabatan'        => $arr->row()->id_jabatan,
+					UNQ . 'nama_jabatan'      => $arr->row()->nama_jabatan,
+					UNQ . 'user_login'        => $arr->row()->user_login,
+					UNQ . 'foto'              => $arr->row()->foto,
+					UNQ . 'flag_ketua_sidang' => $arr->row()->flag_ketua_sidang,
 				];
-				
+
 				$this->session->set_userdata($data_session);
 				$this->session->set_flashdata('first_login', 'Login Berhasil');
-				redirect('admin/rat','refresh');
+				redirect('admin/rat');
 			}
 		}
 	}
@@ -69,18 +70,18 @@ class LoginAdminController extends CI_Controller {
 	public function logout()
 	{
 		$data = [
-			UNQ.'id',
-			UNQ.'nama',
-			UNQ.'id_jabatan',
-			UNQ.'nama_jabatan',
-			UNQ.'user_login',
-			UNQ.'foto',
-			UNQ.'flag_ketua_sidang',
+			UNQ . 'id',
+			UNQ . 'nama',
+			UNQ . 'id_jabatan',
+			UNQ . 'nama_jabatan',
+			UNQ . 'user_login',
+			UNQ . 'foto',
+			UNQ . 'flag_ketua_sidang',
 		];
 
 		$this->session->unset_userdata($data);
 		$this->session->set_flashdata('logout', 'Logout Berhasil');
-		redirect('login/admin','refresh');
+		redirect('login/admin', 'refresh');
 	}
 
 	public function _username_check($username)
@@ -88,9 +89,9 @@ class LoginAdminController extends CI_Controller {
 		$username = $this->_clean_variable($username);
 		$count    = $this->mcore->count('anggota', ['user_login' => $username]);
 
-		if($count == 1){
+		if ($count == 1) {
 			return TRUE;
-		}else{
+		} else {
 			$this->session->set_flashdata('username', $username);
 			$this->session->set_flashdata('username_error', 'Username tidak ditemukan');
 			return FALSE;
@@ -100,12 +101,12 @@ class LoginAdminController extends CI_Controller {
 	public function _password_check($username, $password)
 	{
 		$username    = $this->_clean_variable($username);
-		$password    = $this->_clean_variable($password).UYAH;
+		$password    = $this->_clean_variable($password) . UYAH;
 		$password_db = $this->mcore->get('anggota', 'password_login', ['user_login' => $username], NULL, 'ASC', NULL, NULL);
 
-		if(password_verify($password, $password_db->row()->password_login)){
+		if (password_verify($password, $password_db->row()->password_login)) {
 			return TRUE;
-		}else{
+		} else {
 			$this->session->set_flashdata('password_error', 'Password salah');
 			return FALSE;
 		}
@@ -127,7 +128,7 @@ class LoginAdminController extends CI_Controller {
 		$this->mcore->truncate('polling_pengurus');
 		$this->mcore->truncate('rat');
 		$this->mcore->truncate('respon_rat');
-		$password = password_hash('admin123)'.UYAH, PASSWORD_DEFAULT);
+		$password = password_hash('admin123)' . UYAH, PASSWORD_DEFAULT);
 
 		$data[] = [
 			'nama'              => 'Master Admin',
@@ -189,9 +190,9 @@ class LoginAdminController extends CI_Controller {
 			'flag_ketua_sidang' => 'tidak',
 		];
 
-		for ($i=1; $i <= 10; $i++) { 
+		for ($i = 1; $i <= 10; $i++) {
 			$data[] = [
-				'nama'              => 'Anggota'.$i,
+				'nama'              => 'Anggota' . $i,
 				'tempat_lahir'      => 'Bogor',
 				'tanggal_lahir'     => '2020-03-17',
 				'jenis_kelamin'     => 'laki - laki',
@@ -200,15 +201,15 @@ class LoginAdminController extends CI_Controller {
 				'no_telp'           => '08123456789',
 				'foto'              => 'default.png',
 				'id_jabatan'        => '9',
-				'user_login'        => 'anggota'.$i,
+				'user_login'        => 'anggota' . $i,
 				'password_login'    => $password,
 				'flag_ketua_sidang' => 'tidak',
 			];
 		}
 
-		for ($y=1; $y <= 10; $y++) { 
+		for ($y = 1; $y <= 10; $y++) {
 			$data[] = [
-				'nama'              => 'Karyawan'.$y,
+				'nama'              => 'Karyawan' . $y,
 				'tempat_lahir'      => 'Bogor',
 				'tanggal_lahir'     => '2020-03-17',
 				'jenis_kelamin'     => 'laki - laki',
@@ -217,7 +218,7 @@ class LoginAdminController extends CI_Controller {
 				'no_telp'           => '08123456789',
 				'foto'              => 'default.png',
 				'id_jabatan'        => '9',
-				'user_login'        => 'karyawan'.$y,
+				'user_login'        => 'karyawan' . $y,
 				'password_login'    => $password,
 				'flag_ketua_sidang' => 'tidak',
 			];
@@ -228,9 +229,9 @@ class LoginAdminController extends CI_Controller {
 
 		$exec = $this->mcore->store_batch('anggota', $data);
 
-		if($exec){
+		if ($exec) {
 			echo "Init Complete";
-		}else{
+		} else {
 			echo "Init Fail";
 		}
 	}
